04c3bdd114b8da504895c4155d66ca55dc257929
Alex Terenda <alexterenda@gmail.com>
2021-03-20 11:36:48 +0000

    Created component files

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+
+import Sidebar from "../components/sidebar";
 
 const Dashboard = () => {
   //Insert components on this page
   return (
     <div>
+      <Sidebar />
       <h1>Hello World!</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
