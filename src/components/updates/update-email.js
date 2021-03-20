f78c1ceb934f5b2d56a8bf98368062b5c721a8f4
Damon Roberts <damonspencerroberts@gmail.com>
2021-03-20 19:12:25 +0000

    form submit send data to dashboard before patch

--- src/components/updates/update-email.js
+++ src/components/updates/update-email.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const UpdateEmail = (props) => {
+  const [formData, setFormData] = useState({
+    date: "",
+    endpoint: "",
+    source: "",
+    id: ""
+  });
+
+  const handleFormData = (el, type, id) => {
+    const newFormData = {...formData};
+    newFormData[type] = el;
+    newFormData.id = id;
+    setFormData(newFormData);
+  }
+
   return (
     <div>
       <div>
-        <input type="text" placeholder="Date" />
-        <input type="text" placeholder="Endpoint" />
-        <input type="text" placeholder="Source" />
-        <button>Update</button>
+        <input value={formData.date} type="text" placeholder="Date" onChange={(e) => handleFormData(e.currentTarget.value, "date", props.id)}/>
+        <input value={formData.endpoint} type="text" placeholder="Endpoint" onChange={(e) => handleFormData(e.currentTarget.value, "endpoint", props.id)} />
+        <input value={formData.source} type="text" placeholder="Source" onChange={(e) => handleFormData(e.currentTarget.value, "source", props.id)} />
+        <button onClick={() => props.handleFormDataSubmit(formData)}>Update</button>
       </div>
       <div>
         <p>{props.text}</p>
