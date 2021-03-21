426d3d12790e442622da0808248e9976c0d4fac7
Alex Terenda <alexterenda@gmail.com>
2021-03-21 06:51:26 +0000

    Continued search styles

--- src/components/search-bar/search.js
+++ src/components/search-bar/search.js
@@ -14,7 +14,7 @@ const Search = () => {
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
-          stroke-width="1.5"
+          stroke-width="2"
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
       </svg>
