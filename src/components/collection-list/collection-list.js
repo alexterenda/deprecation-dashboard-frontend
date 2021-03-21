8ce0ed76785a07ad203eb79e69cbf6f72b0803d7
Alex Terenda <alexterenda@gmail.com>
2021-03-21 05:52:59 +0000

    Added collection list styles

--- src/components/collection-list/collection-list.js
+++ src/components/collection-list/collection-list.js
@@ -4,25 +4,43 @@ import CollectionListItem from "./collection-list-item";
 const CollectionList = () => {
   const collectionListItems = [
     {
+      tag: "GA",
+      class: "collection-list-item-1",
       name: "GraphQL APIs",
       itemCount: "6",
     },
     {
+      tag: "E",
+      class: "collection-list-item-2",
       name: "Ecommerce",
       itemCount: "28",
     },
     {
+      tag: "P",
+      class: "collection-list-item-3",
       name: "Payments",
       itemCount: "5",
     },
   ];
 
   const createCollectionListItems = collectionListItems.map((item) => {
-    return <CollectionListItem name={item.name} itemCount={item.itemCount} />;
+    return (
+      <CollectionListItem
+        name={item.name}
+        itemCount={item.itemCount}
+        tag={item.tag}
+        class={item.class}
+      />
+    );
   });
 
   return (
-    <div className="collection-list-container">{createCollectionListItems}</div>
+    <div className="collection-list-container">
+      <h2 className="collection-list-heading">Collections</h2>
+      <div className="collection-list-items-container">
+        {createCollectionListItems}
+      </div>
+    </div>
   );
 };
 
