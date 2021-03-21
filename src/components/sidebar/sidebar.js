1489756430ad0fef3d34878b8123a2322d1241fc
Alex Terenda <alexterenda@gmail.com>
2021-03-21 04:36:11 +0000

    Added toggle icon, changed stroke-width

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -62,7 +62,7 @@ const SideBar = () => {
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
+                    stroke-width="1.5"
                     d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
                   />
                 </svg>
@@ -71,6 +71,22 @@ const SideBar = () => {
                 <a href="/" className="sidebar-sidelink-text">
                   My APIs
                 </a>
+              </div>
+              <div className="sidebar-sidelink-toggle-icon-container">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="sidebar-sidelink-toggle-icon"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M5 15L12 8L19 15"
+                  />
+                </svg>
               </div>
             </div>
             <div className="sidebar-menulinks-container">{createApiLinks}</div>
@@ -89,7 +105,7 @@ const SideBar = () => {
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
+                    stroke-width="1.5"
                     d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
                   />
                 </svg>
@@ -98,6 +114,22 @@ const SideBar = () => {
                 <a href="/" className="sidebar-sidelink-text">
                   Collections
                 </a>
+              </div>
+              <div className="sidebar-sidelink-toggle-icon-container">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="sidebar-sidelink-toggle-icon"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M5 15L12 8L19 15"
+                  />
+                </svg>
               </div>
             </div>
             <div className="sidebar-menulinks-container">
